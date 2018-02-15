dce58ab83dcbfa9dde1002e2bb05882a4091cc97
Pavel Azanov <pavel.azanov@pb.com>
2018-02-15 08:07:07 +0100

    fix(url): use url api instead of regex to check urls due to redos vulnerability

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*
 	Universal validation plugin
-	(c) 2014 - 2016 Pavel Azanov, developed for CSSSR
+	(c) 2014 - 2018 Pavel Azanov, developed for CSSSR
 
-	Version: 0.0.24
+	Version: 0.0.25
 	----
 
 	Using parts of jQuery.bind-first (https://github.com/private-face/jquery.bind-first)
@@ -11,15 +11,15 @@
 (function (factory) {
 	if (typeof define === 'function' && define.amd) {
 		// AMD. Register as an anonymous module.
-		define(['jquery'], factory);
+		define(['jquery', 'url'], factory);
 	} else if (typeof exports === 'object') {
 		// Node/CommonJS
-		module.exports = factory(require('jquery'));
+		module.exports = factory(require('jquery'), require('url'));
 	} else {
 		// Browser globals
-		factory(jQuery);
+		factory(jQuery, URL);
 	}
-}(function ($) {
+}(function ($, URL) {
 
 	function moveHandlerToTop($el, eventName, isDelegated) {
 
@@ -188,7 +188,14 @@
 			},
 			patterns: {
 				email: /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i,
-				url: /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
+				url: function (value) {
+					try {
+						var url = new URL(value);
+						return url.protocol === 'https:' || url.protocol === 'http:';
+					} catch (ex) {
+						return false;
+					}
+				}
 			},
 			inputPatterns: {
 				cyrillic: /[а-яёії\s]/i,
